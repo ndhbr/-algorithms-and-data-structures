3c6d507fefc5eb66a9cc5dbe8d789a16ec319a3c
Andreas Huber <andi@pl32.de>
2019-10-02 16:47:59 +0200

    Initial commit

---
+++ blatt1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace blatt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ggt(32, 33);
+            ggt(32, 33, 0);
+        }
+
+        public void ggt(int a, int b)
+        {
+            int r = 0;
+
+            do
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            } while (r != 0);
+
+            Console.WriteLine("Größter gemeinsamer Teiler: " + a);
+        }
+
+        public static int ggtRecursive(int a, int b, int r)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+
+            if(r == 0)
+            {
+                Console.WriteLine("Größter gemeinsamer Teiler: " + a);
+                return a;
+            }
+            else
+            {
+                return ggtRecursive(a, b, r);
+            }
+        }
+    }
+}
---
+++ hello-world/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ggt(33, 21);
+            ggtRecursive(33, 21, 0);
+        }
+
+        public static void ggt(int a, int b)
+        {
+            int r = 0;
+
+            do
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            } while (r != 0);
+
+            Console.WriteLine("Größter gemeinsamer Teiler: " + a);
+        }
+
+        public static int ggtRecursive(int a, int b, int r)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+
+            if(r == 0)
+            {
+                Console.WriteLine("Größter gemeinsamer Teiler: " + a);
+                return a;
+            }
+            else
+            {
+                return ggtRecursive(a, b, r);
+            }
+        }
+    }
+}
