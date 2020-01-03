cc34a6870047b03b47494965e87ad5c92b2b52bd
Andreas Huber <andreashuber@Andreass-iMac.fritz.box>
2020-01-03 20:05:03 +0100

    Tried to make an exercise, but to lazy

---
+++ exercise-sheet-12/Exercise2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace exercise_sheet_12
+{
+    class Exercise2
+    {
+        public Exercise2()
+        {
+
+        }
+    }
+
+    class PriorityQueue
+    {
+        public PriorityQueue()
+        {
+
+        }
+    }
+
+    class MinHeap
+    {
+        private List<int> array = new List<int>();
+    }
+}
---
+++ exercise-sheet-12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise_sheet_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise2 ex2 = new Exercise2();
+        }
+    }
+}
